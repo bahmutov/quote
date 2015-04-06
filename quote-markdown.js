4301cc8d71ce06794a6b11dd976eda01f4bd4bc8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2015-04-06 13:47:40 -0400

    removed quote-markdown files

--- quote-markdown.js
+++
@@ -1,8 +0,0 @@
-var quote = require('./quote');
-
-module.exports = {
-  string: quote({ quotes: '"' }),
-  code: quote({ quotes: '`' }),
-  em: quote({ quotes: '*' }),
-  bold: quote({ quotes: '**' })
-};
